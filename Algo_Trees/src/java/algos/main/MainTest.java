592eb146b27a313540b692e6adf318412b67236a
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-07-06 20:50:03 -0500

    Filling out the tree

--- Algo_Trees/src/java/algos/main/MainTest.java
+++ Algo_Trees/src/java/algos/main/MainTest.java
@@ -7,6 +7,16 @@ public class MainTest {
 
 	public static void main(String[]...args){
 		MyTreeNode root = new MyTreeNode(7);
+		root.setLeft(new MyTreeNode(3));
+		root.getLeft().setLeft(new MyTreeNode(20));
+		root.getLeft().setRight(new MyTreeNode(21));
+		root.getLeft().getRight().setLeft(new MyTreeNode(7));
+		root.getLeft().getRight().setRight(new MyTreeNode(8));
+
+		root.setRight(new MyTreeNode(1));
+		root.getRight().setRight(new MyTreeNode(10));
+		root.getRight().getRight().setLeft(new MyTreeNode(5));
+
 		MyTree tree = new MyTree(root);
 	}
 }
