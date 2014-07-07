da55d74721cb4a0310b132fcbb2a5af9c6b02738
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-07-06 20:36:02 -0500

    Resursion Traversal of trees

---
+++ Algo_Trees/src/java/algos/main/MainTest.java
@@ -0,0 +1,12 @@
+package java.algos.main;
+
+import java.algos.data.MyTree;
+import java.algos.data.MyTree.MyTreeNode;
+
+public class MainTest {
+
+	public static void main(String[]...args){
+		MyTreeNode root = new MyTreeNode(7);
+		MyTree tree = new MyTree(root);
+	}
+}
---
+++ Algo_Trees/src/java/algos/traversal/RecursionTraversal.java
@@ -0,0 +1,43 @@
+package java.algos.traversal;
+
+import java.algos.data.MyTree.MyTreeNode;
+
+/*
+ * A static class for printing Tree values
+ * Recursion Traversal of Tree using different algorithms
+ */
+public class RecursionTraversal {
+
+	/*
+	 * Visit Left Node, Current Node, then Right Node
+	 */
+	public static void inOrderTraversal(MyTreeNode root){
+		if(root != null){
+			inOrderTraversal(root.getLeft());
+			System.out.println(" "+root.getData());
+			inOrderTraversal(root.getRight());
+		}
+	}
+	
+	/*
+	 * Visit Current Node, Left Node, then Right Node
+	 */
+	public static void preOrderTraversal(MyTreeNode root){
+		if(root != null){
+			System.out.println(" "+root.getData());
+			preOrderTraversal(root.getLeft());
+			preOrderTraversal(root.getRight());
+		}
+	}
+	
+	/*
+	 * Visit Left Node, Right Node, then Current Node
+	 */
+	public static void postOrderTraversal(MyTreeNode root){
+		if(root != null){
+			postOrderTraversal(root.getLeft());
+			postOrderTraversal(root.getRight());
+			System.out.println(" "+root.getData());
+		}
+	}
+}
