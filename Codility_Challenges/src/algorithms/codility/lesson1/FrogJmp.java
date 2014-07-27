2efb75b75a4a6bbc35bdf791992539c13e0c04fc
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-07-27 02:09:08 -0500

    Added Frog Jump codility challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson1/FrogJmp.java
@@ -0,0 +1,16 @@
+package algorithms.codility.lesson1;
+
+public class FrogJmp {
+
+	public int solution(int X, int Y, int D) {
+		if (X > Y)
+			return 0;
+
+		// Number of Jumps is equal to the difference between two position,
+		// and divided it by the fixed distance a frog can jump
+		double diff = Y - X;
+		double numOfJumbs = Math.ceil(diff / D);
+
+		return (int) numOfJumbs;
+	}
+}
