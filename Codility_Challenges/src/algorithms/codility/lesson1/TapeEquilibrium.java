90113ff7cde4f24436709c59a1fe2cacc87017b5
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-07-27 01:38:40 -0500

    Added Tape equilibrium Codility lesson's one challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson1/TapeEquilibrium.java
@@ -0,0 +1,28 @@
+package algorithms.codility.lesson1;
+
+public class TapeEquilibrium {
+
+    // Method to calculate minimum difference between two arrays.
+    public int solution(int[] A) {
+        if(A.length == 0 || A.length == 1)
+            return 0;
+    
+        int totalSum = 0;
+        // Loop to calculate Total Sum of an Array.    
+        for(int i = 0 ; i < A.length ; i++){
+            totalSum += A[i];
+        }
+        
+        int minDiff = Integer.MAX_VALUE;
+        int tempSum = 0;
+        for(int i = 0 ; i < A.length -1 ; i++){
+            tempSum += A[i];
+            totalSum -= A[i];
+            if(Math.abs(tempSum - totalSum) < minDiff){
+                minDiff = Math.abs(tempSum - totalSum);
+            }
+        }
+        return minDiff;
+    }
+	
+}
