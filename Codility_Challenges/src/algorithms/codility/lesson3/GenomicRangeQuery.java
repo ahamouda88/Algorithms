26151d245ba464d52e8a9b76394bdb94e2c85e61
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-07 20:41:46 -0500

    Adding Genomic Range Query challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson3/GenomicRangeQuery.java
@@ -0,0 +1,34 @@
+package algorithms.codility.lesson3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenomicRangeQuery {
+	
+	public static int[] solution(String S, int[] P, int[] Q) {
+        int[] result = new int[P.length];
+        Map<Character, Integer> nucleotides = new HashMap<>();
+        nucleotides.put('A',1);
+        nucleotides.put('C',2);
+        nucleotides.put('G',3);
+        nucleotides.put('T',4);
+        
+        int[] factors = new int[S.length()];
+        // Loop to replace nucleotide types with impact factors
+        for(int i = 0 ; i < S.length() ; i++){
+            factors[i] = nucleotides.get(S.charAt(i));
+        }
+        
+        int min;
+        for(int i = 0 ; i < P.length ; i++){
+            min = 5;
+            for(int j = P[i] ; j <= Q[i] ; j++){
+                if(min > factors[j]){
+                    min = factors[j];
+                }
+            }
+            result[i] = min;
+        }
+        return result;
+    }
+}
