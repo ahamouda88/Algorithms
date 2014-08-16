af34e85285d5f49263cdbf056b015ea52a3a9604
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-15 23:31:49 -0500

    Adding a new sorting challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson4/Distinct.java
@@ -0,0 +1,32 @@
+package algorithms.codility.lesson4;
+
+import java.util.Arrays;
+
+public class Distinct {
+
+	/**
+	 * A Method to compute number of distinct values in an array.
+	 * @param A
+	 * @return
+	 */
+	public static int solution(int[] A) {
+
+		// No elements in the Array
+		if (A.length == 0) {
+			return 0;
+		}
+		int counter = 1;
+
+		// Sort the array
+		Arrays.sort(A);
+
+		// Iterate and count the number of distinct values
+		for (int i = 1; i < A.length; i++) {
+			if (A[i - 1] != A[i]) {
+				counter++;
+			}
+		}
+
+		return counter;
+	}
+}
