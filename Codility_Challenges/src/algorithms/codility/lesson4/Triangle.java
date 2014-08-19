0a4e48ee07a82eaa9ef28cc7c4ed39ce31582ab9
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-18 19:28:29 -0500

    simple change to the Triangle challenge

--- Codility_Challenges/src/algorithms/codility/lesson4/Triangle.java
+++ Codility_Challenges/src/algorithms/codility/lesson4/Triangle.java
@@ -11,6 +11,9 @@ public class Triangle {
 	 */
 	public static int solution(int[] A) {
 		
+		if(A.length < 3)
+			return 0;
+		
 		Arrays.sort(A);
 		for (int i = 0; i < A.length - 2; i++) {
 			if (A[i] + A[i + 1] > A[i + 2])
