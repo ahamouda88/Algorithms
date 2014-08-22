a5f6727796dd6763e1a6e6712ec8b9d336b2a1b9
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-21 22:51:56 -0500

    Added some comments to MaxProductOfThree Challenge

--- Codility_Challenges/src/algorithms/codility/lesson4/MaxProductOfThree.java
+++ Codility_Challenges/src/algorithms/codility/lesson4/MaxProductOfThree.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class MaxProductOfThree {
 
+	/**
+	 * A Method to maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R). 
+	 * @param A
+	 * @return
+	 */
 	public static int solution(int[] A) {
 		int n = A.length;
 		
@@ -12,6 +17,8 @@ public class MaxProductOfThree {
 		
 		Arrays.sort(A);
 		
+		// Maximum product will be either the last three elements
+		// Or the first two elements in case of having large negative numbers, with the last element in the array
 		int max = A[n - 1] * A[n - 2] * A[n - 3];
 		if (max < A[0] * A[1] * A[n - 1])
 			max = A[0] * A[1] * A[n - 1];
