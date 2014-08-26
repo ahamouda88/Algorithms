e952a9f533e4f8c3472b27cdb506ed2a425e1d72
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-25 21:57:16 -0500

    first implementation for the number of disc intersections challenge

--- Codility_Challenges/src/algorithms/codility/lesson4/NumberOfDiscIntersections.java
+++ Codility_Challenges/src/algorithms/codility/lesson4/NumberOfDiscIntersections.java
@@ -4,11 +4,26 @@ public class NumberOfDiscIntersections {
 	
 	/**
 	 * A Method to compute intersections between sequence of discs. 
+	 * Time Complexity: O(n2)
 	 * @param A
 	 * @return
 	 */
 	public static int solution(int[] A) {
-		return 0;
+        int n = A.length;
+        int count = 0;
+        for(int i = 0 ; i < n - 1 ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                int x = Math.max(i,j);
+                int y = Math.min(i,j);
+                if((x - A[x]) <= (y + A[y])){
+                    count++;
+                }
+            }
+        }
+        if(count > 10000000){
+            return -1;
+        }
+        return count;
 	}
 
 }
