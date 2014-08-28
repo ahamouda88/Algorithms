e102b611b69df18b8db184585f27ac0b7ce55795
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-08-28 00:13:50 -0500

    Fixing some issues in the brackets callenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson5/Brackets.java
@@ -0,0 +1,42 @@
+package algorithms.codility.lesson5;
+
+import java.util.Stack;
+
+public class Brackets {
+	public int solution(String S) {
+
+		Stack<Character> stack = new Stack<>();
+
+		// That means one bracket doesn't have its closing bracket.
+		if (S.length() % 2 != 0)
+			return 0;
+
+		if (S.isEmpty())
+			return 1;
+
+		for (int i = 0; i < S.length(); i++) {
+			Character ch = S.charAt(i);
+			boolean matchFound = false;
+			if (ch == '{' || ch == '(' || ch == '[') {
+				stack.push(ch);
+			} else {
+				if(ch == '}' && stack.peek() == '{')
+					matchFound = true;
+				else if(ch == ']' && stack.peek() == '[')	
+					matchFound = true;
+				else if(ch == ')' && stack.peek() == '(')
+					matchFound = true;
+				
+				if(matchFound){
+					stack.pop();
+				}else{
+					return 0;
+				}
+			}
+		}
+		if(!stack.isEmpty())
+			return 0;
+		
+		return 1;
+	}
+}
