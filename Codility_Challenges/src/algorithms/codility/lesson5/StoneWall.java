e3099c9cb275a7f74d1d5791ddeadac1de04dacc
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-09-03 23:05:55 -0500

    Adding StoneWall challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson5/StoneWall.java
@@ -0,0 +1,14 @@
+package algorithms.codility.lesson5;
+
+public class StoneWall {
+	
+	/**
+	 * A Method to cover "Manhattan skyline" using the minimum number of rectangles. 
+	 * @param H
+	 * @return
+	 */
+	public static int solution(int[] H) {
+       return 0;
+    }
+
+}
