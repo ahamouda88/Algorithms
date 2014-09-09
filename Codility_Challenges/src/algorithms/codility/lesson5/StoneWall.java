cd7fd045a6d6d0dd9b8935cb0d206e63b57e6844
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-09-08 19:52:22 -0500

    Adding implementation for StoneWall challenge

--- Codility_Challenges/src/algorithms/codility/lesson5/StoneWall.java
+++ Codility_Challenges/src/algorithms/codility/lesson5/StoneWall.java
@@ -1,14 +1,27 @@
 package algorithms.codility.lesson5;
 
+import java.util.Stack;
+
 public class StoneWall {
-	
+
 	/**
 	 * A Method to cover "Manhattan skyline" using the minimum number of rectangles. 
 	 * @param H
 	 * @return
-	 */
+	 */	
 	public static int solution(int[] H) {
-       return 0;
-    }
+		Stack<Integer> stack = new Stack<>();
+		int counter = 0;
+		for (int i = 0 ; i < H.length ; i++) {
+			while ( !stack.isEmpty() && stack.peek() > H[i]) {
+				stack.pop();
+			}
+			if (!(!stack.isEmpty() && stack.peek() == H[i])) {
+				stack.push(H[i]);
+				counter++;
+			}
+		}
+		return counter;
+	}
 
 }
