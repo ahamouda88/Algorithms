2ac8669dd9f51f6f12b571874aefc0c1bc57b14f
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-09-12 20:31:29 -0500

    Fix some cases in the dominator challenge

--- Codility_Challenges/src/algorithms/codility/lesson6/Dominator.java
+++ Codility_Challenges/src/algorithms/codility/lesson6/Dominator.java
@@ -3,34 +3,39 @@ package algorithms.codility.lesson6;
 public class Dominator {
 
 	/**
-	 * A Method to find an index of an array such that its value occurs at more than half of indices in the array.
+	 * A Method to find an index of an array such that its value occurs at more
+	 * than half of indices in the array.
+	 * 
 	 * @param A
 	 * @return
 	 */
 	public static int solution(int[] A) {
 		int len = A.length;
 		if (len <= 0) {
-			return 0;
+			return -1;
 		}
 		int leader = A[0];
 		int counter = 0;
 		/* Find the Leader */
-		for (int i = 1; i < len; i++) {
-			if (A[i] == leader)
+		for (int i = 0; i < len; i++) {
+			if (A[i] == leader) {
 				counter++;
-			else
-				counter--;
-
-			if (counter == 0) {
-				leader = A[i];
-				counter++;
+			} else {
+				if (counter == 0) {
+					leader = A[i];
+					counter++;
+				} else {
+					counter--;
+				}
 			}
 		}
+		
+		/*
+		 * Check that the value of the leader occurs more than half of values in
+		 * the array
+		 */
 		counter = 0;
 		int lastIndex = -1;
-		/*
-		 * Check that the value of the leader occurs more than half of values in the array
-		 */
 		for (int i = 0; i < len; i++) {
 			if (A[i] == leader) {
 				counter++;
