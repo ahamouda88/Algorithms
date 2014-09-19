576a194ed5047efae2476ca3cbf1ed19ff9013e7
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-09-18 22:59:42 -0500

    Remove unnecessary code

--- Algo_Trees/src/algorithms/tree/main/Main.java
+++ Algo_Trees/src/algorithms/tree/main/Main.java
@@ -1,18 +1,8 @@
 package algorithms.tree.main;
 
 public class Main {
-
-	private int x;
-	public void m(String...d){
-		
-	}
+	
 	public static void main(String[] args){
-		int a = 1;
-		int b = 2;
-		int c = ++a + b--;
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
 		Interview interview = new Interview();
 		interview.loadCandidates();
 		interview.setupAndPrintCandidateData();
