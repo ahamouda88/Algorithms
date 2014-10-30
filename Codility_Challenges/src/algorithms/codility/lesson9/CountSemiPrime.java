2b1d141ba90d5ff2c702b559572a2c1cbcc81db2
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-10-29 23:56:50 -0500

    Add comments

--- Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
+++ Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
@@ -4,12 +4,15 @@ import java.util.*;
 
 class CountSemiPrime {
 
+	// Still not working, and need some refactor
 	public int[] solution(int N, int[] P, int[] Q) {
 		boolean[] sieve = new boolean[N + 1];
 		int[] result = new int[P.length];
 		for (int i = 2; i < sieve.length; i++) {
 			sieve[i] = true;
 		}
+		
+		// Mark all prime numbers.
 		int x = 2;
 		while (x * x <= N) {
 			if (sieve[x] == true) {
@@ -22,7 +25,7 @@ class CountSemiPrime {
 			x++;
 		}
 
-		// Get All semiprime
+		// Get All semi-prime numbers.
 		Set<Integer> set = new HashSet<Integer>();
 		int[] countsemi = new int[N + 1];
 		int count = 0;
