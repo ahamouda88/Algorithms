652c79a85f63797d9706840093b5d9e9a7393acb
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-10-30 00:22:08 -0500

    Refactor count semiprime challenge

--- Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
+++ Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
@@ -1,23 +1,29 @@
 package algorithms.codility.lesson9;
 
-import java.util.*;
-
 class CountSemiPrime {
 
-	// Still not working, and need some refactor
+	/**
+	 * A Method to count the semiprime numbers in the given range [a..b] 
+	 * @param N
+	 * @param P
+	 * @param Q
+	 * @return
+	 */
 	public static int[] solution(int N, int[] P, int[] Q) {
-		boolean[] sieve = new boolean[N + 1];
+		int length = N+1;
+		boolean[] sieve = new boolean[length];
 		int[] result = new int[P.length];
-		for (int i = 2; i < sieve.length; i++) {
+		
+		for (int i = 2; i < length; i++) {
 			sieve[i] = true;
 		}
 		
 		// Mark all prime numbers.
 		int x = 2;
-		while (x * x <= N) {
+		while (x * x < length) {
 			if (sieve[x] == true) {
 				int k = x * x;
-				while (k <= N) {
+				while (k < length) {
 					sieve[k] = false;
 					k += x;
 				}
@@ -25,11 +31,11 @@ class CountSemiPrime {
 			x++;
 		}
 
-		// Get All semi-prime numbers.
+		// Get all semi-prime numbers.
 		int[] countsemi = new int[N + 1];
-        for(int i=2 ; i < N + 1; i++){
+        for(int i=2 ; i < length; i++){
             if(sieve[i] == true){
-                for(int j=i ; j * i <= N; j++){
+                for(int j=i ; j * i < length; j++){
                     if(sieve[j] == true){
                         countsemi[i * j] = 1; 
                     }
@@ -37,8 +43,9 @@ class CountSemiPrime {
             }
         }
         
+        // Count semi-prime number at each index.
         int count = 0;
-        for(int i=0 ; i < N+1 ; i++){
+        for(int i=0 ; i < length ; i++){
         	count += countsemi[i];
         	countsemi[i] = count;
         }
@@ -52,7 +59,4 @@ class CountSemiPrime {
 		return result;
 	}
 	
-	public static void main(String[] args){
-		CountSemiPrime.solution(26, new int[]{4}, new int[]{10});
-	}
 }
