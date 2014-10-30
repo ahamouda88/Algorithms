df6dfc8aaf55df47af2522b7c5869d74db4d653f
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-10-30 00:52:52 -0500

    Made some changes to count semiprime challenge

--- Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
+++ Codility_Challenges/src/algorithms/codility/lesson9/CountSemiPrime.java
@@ -10,20 +10,20 @@ class CountSemiPrime {
 	 * @return
 	 */
 	public static int[] solution(int N, int[] P, int[] Q) {
-		int length = N+1;
-		boolean[] sieve = new boolean[length];
+		
+		boolean[] sieve = new boolean[N + 1];
 		int[] result = new int[P.length];
 		
-		for (int i = 2; i < length; i++) {
+		for (int i = 2; i <= N; i++) {
 			sieve[i] = true;
 		}
 		
 		// Mark all prime numbers.
 		int x = 2;
-		while (x * x < length) {
+		while (x * x <= N) {
 			if (sieve[x] == true) {
 				int k = x * x;
-				while (k < length) {
+				while (k <= N) {
 					sieve[k] = false;
 					k += x;
 				}
@@ -33,27 +33,22 @@ class CountSemiPrime {
 
 		// Get all semi-prime numbers.
 		int[] countsemi = new int[N + 1];
-        for(int i=2 ; i < length; i++){
-            if(sieve[i] == true){
-                for(int j=i ; j * i < length; j++){
-                    if(sieve[j] == true){
-                        countsemi[i * j] = 1; 
+        for(long i=2 ; i <= N; i++){
+            if(sieve[(int)i] == true){
+                for(long j=i ; j * i <= N; j++){
+                    if(sieve[(int)j] == true){
+                        countsemi[(int)(i * j)] = 1; 
                     }
                 }
             }
         }
         
         // Count semi-prime number at each index.
-        int count = 0;
-        for(int i=0 ; i < length ; i++){
-        	count += countsemi[i];
-        	countsemi[i] = count;
+        for(int i=1 ; i <= N ; i++){
+        	countsemi[i] += countsemi[i - 1];
         }
         
         for(int i=0; i < P.length ; i++){
-        	for(int j = P[i] ; j <= Q[i] ; j++){
-        		count += countsemi[j];
-        	}
         	result[i] = countsemi[Q[i]] - countsemi[P[i] - 1];
         }
 		return result;
