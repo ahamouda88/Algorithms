a8cdae3757f796a9b721fc5714aaaf705a042148
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-10-31 19:16:35 -0500

    Add some changes to the countnondivisible challenge but still not working. Working on another algorithm better than this

--- Codility_Challenges/src/algorithms/codility/lesson9/CountNonDivisible.java
+++ Codility_Challenges/src/algorithms/codility/lesson9/CountNonDivisible.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountNonDivisible {
-	
-	public static int[] solution(int[] A){
+
+	// This Algorithm is Not working!
+	public static int[] solution(int[] A) {
 		int n = A.length;
 		Map<Integer, Integer> map = new HashMap<>();
 		// Count number of existence for each element in the array.
@@ -13,28 +14,40 @@ public class CountNonDivisible {
 			int x = map.get(A[i]) == null ? 1 : map.get(A[i]) + 1;
 			map.put(A[i], x);
 		}
-		
+
+		int[] result = new int[n];
 		// Each number is divisible by itself
 		int[] counter = new int[n];
 		for (int i = 0; i < n; i++) {
-			counter[i] = 1;
+			int one = map.get(A[i]) == 1 ? 0 : 1;
+			counter[i] = map.get(A[i]) + one;
+		}
+
+		for (int  i= 0; i < n; i++) {
+			int j = 2;
+			while (j <= Math.sqrt(A[i]) + 1) {
+					if (map.get(j) != null && A[i] % j == 0) {
+						counter[i] += map.get(j);
+					}
+				j++;
+			}
 		}
 		
 		// Count number of divisible for each element in the array.
 		for (int i = 0; i < n; i++) {
 			for (int j = 1; j <= Math.ceil(Math.sqrt(A[i])); j++) {
-				if(map.get(j) != null && A[i] % j == 0){
+				if (map.get(j) != null && A[i] % j == 0) {
 					counter[i] += map.get(j);
 				}
 			}
 		}
-		return null;
+		return result;
 	}
-	
-	public static void main(String[] args){
-		int[] input = new int[]{3,1,2,3,6};
+
+	public static void main(String[] args) {
+		int[] input = new int[] { 3, 1, 2, 3, 6 };
 		CountNonDivisible.solution(input);
-		
+
 	}
 
 }
