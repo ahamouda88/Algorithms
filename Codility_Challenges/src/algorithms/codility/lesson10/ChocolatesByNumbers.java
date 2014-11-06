46aec361af330f79ee642c9c6d6dccd8f81869f0
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-05 20:51:12 -0600

    Adding comments

--- Codility_Challenges/src/algorithms/codility/lesson10/ChocolatesByNumbers.java
+++ Codility_Challenges/src/algorithms/codility/lesson10/ChocolatesByNumbers.java
@@ -2,13 +2,19 @@ package algorithms.codility.lesson10;
 
 public class ChocolatesByNumbers {
 
-	
+	/**
+	 * There are N chocolates in a circle. Count the number of chocolates you will eat. 
+	 * @param N
+	 * @param M
+	 * @return
+	 */
 	public int solution(int N, int M) {
 		long gcd = getGCD(N, M);
 		long lcm = ((long)N * M) / gcd;
 		return (int)(lcm / M);
 	}
 
+	// Using Euclidean algorithm to get the Greatest Common Divisor.
 	private long getGCD(int a, int b) {
 		if (a % b ==0) {
 			return b;
