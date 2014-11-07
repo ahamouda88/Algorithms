d8a164403365f4e2f2147dd996f7968429ce2bc6
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-06 21:57:46 -0600

    Add a tree function to convert sorted array to BST

---
+++ Algo_Trees/src/algorithms/tree/funtions/SortedArraytoBST.java
@@ -0,0 +1,24 @@
+package algorithms.tree.funtions;
+
+import algorithms.tree.data.MyTreeNode;
+
+public class SortedArraytoBST {
+	
+	/**
+	 * A Method to convert a sorted array of integers to a Binary Search Tree.
+	 * @param array
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	public static MyTreeNode sortedArrayToBST(int[] array, int start, int end){
+		if(start > end){
+			return null;
+		}
+		int mid = start + (end - start) / 2;
+		MyTreeNode node = new MyTreeNode(array[mid]);
+		node.setLeft(sortedArrayToBST(array, start, mid - 1));
+		node.setRight(sortedArrayToBST(array, mid + 1, end));
+		return node;
+	}
+}
