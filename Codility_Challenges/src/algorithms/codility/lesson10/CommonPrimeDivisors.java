1705f5562042c256c03b48626f2e5fff275a1644
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-07 21:22:08 -0600

    Add Commonprimedivisors challenge

---
+++ Codility_Challenges/src/algorithms/codility/lesson10/CommonPrimeDivisors.java
@@ -0,0 +1,43 @@
+package algorithms.codility.lesson10;
+
+public class CommonPrimeDivisors {
+
+	public static int solution(int[] A, int[] B) {
+		int n = A.length;
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (hasSameDivisors(A[i], B[i])) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static boolean hasSameDivisors(int a, int b) {
+		int gcd = getGCD(a, b);
+
+		int x = 0;
+		int y = 0;
+		while (x != 1) {
+			x = getGCD(a, gcd);
+			a = a / x;
+		}
+		if (a != 1)
+			return false;
+		while (y != 1) {
+			y = getGCD(b, gcd);
+			b = b / y;
+		}
+		if (b != 1)
+			return false;
+		return true;
+	}
+
+	private static int getGCD(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		} else {
+			return getGCD(b, a % b);
+		}
+	}
+}
