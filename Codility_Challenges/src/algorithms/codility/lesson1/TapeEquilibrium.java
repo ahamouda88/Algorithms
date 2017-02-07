5f5f373794cc1d47460a27b13eee0ba87817833d
Ahmed Hamouda <ahmed.hamouda8888@gmail.com>
2017-02-07 18:11:29 -0500

    update tapeEquilibrium function

--- Codility_Challenges/src/algorithms/codility/lesson1/TapeEquilibrium.java
+++ Codility_Challenges/src/algorithms/codility/lesson1/TapeEquilibrium.java
@@ -2,28 +2,26 @@ package algorithms.codility.lesson1;
 
 public class TapeEquilibrium {
 
-	/**
-	 * A Method to calculate minimum difference between two arrays.
-	 * @param A
-	 * @return
-	 */
-    public static int solution(int[] A) {
-        if(A.length == 0 || A.length == 1)
-            return 0;
-    
-        int totalSum = 0;
-        // Loop to calculate Total Sum of an Array.    
-        for(int i = 0 ; i < A.length ; i++){
-            totalSum += A[i];
+   /**
+    * A Method to calculate minimum difference between two arrays.
+    * @param A
+    * @return
+    */
+    public int solution(int[] a) {
+        int length = a.length;
+        int[] sums = new int[length];
+        
+        sums[0] = a[0];
+        for(int i=1 ; i<length ; i++){
+            sums[i] = sums[i-1] + a[i]; 
         }
         
         int minDiff = Integer.MAX_VALUE;
-        int tempSum = 0;
-        for(int i = 0 ; i < A.length -1 ; i++){
-            tempSum += A[i];
-            totalSum -= A[i];
-            if(Math.abs(tempSum - totalSum) < minDiff){
-                minDiff = Math.abs(tempSum - totalSum);
+        for(int i=1 ;i<length ; i++){
+            int end = sums[length - 1] - sums[i-1];
+            int diff = Math.abs(sums[i-1] - end);
+            if(diff < minDiff){
+                minDiff = diff;
             }
         }
         return minDiff;
